1ef27ebcfcda25adb5dbd2e4a3e759d7462c5aa8
Abhisheke01 <124560403+Abhisheke01@users.noreply.github.com>
2023-04-19 12:05:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
-
-const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
